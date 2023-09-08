c1939aa68925d366b5dced23a828f78bc0cd7aae
unknown <you@example.com>
2023-09-08 23:20:19 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+let coco = ["ahmed",22,"mohamed",55,"Ali","eid"];
+let dodo = []
+let zozo = []
+
+
+
+// if (typeof coco[0] === "string") {
+//     dodo.push(coco[0]);
+// }
+
+// if (typeof coco[1] === "string") {
+//     dodo.push(coco[1]);
+// }
+
+// if (typeof coco[2] === "string") {
+//     dodo.push(coco[2])
+// }
+
+for (let i = 0; i < coco.length; i++) {
+    if (typeof coco[i] === "string") {
+             dodo.push(coco[i]);
+             
+         }else{
+            zozo.push(coco[i])
+         }
+    
+}
+
+
+
+
+console.log(dodo);
+console.log(zozo);
+
+var family = ["adam",2015, "eid",1987, "eman", "rital",2021, "mahamd", "ahmad", "aslam", "mahmod", "yosef"]
+
+for (let i = 0; i < family.length; i++) {
+family[i]
+}
+
+
+var adg =["", "", "", "1980", "1980", "1991", "", "", "", "",]
